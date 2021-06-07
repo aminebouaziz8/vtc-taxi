a994a6b371945513163742dc9d2c3fa7a8e519f8
aminebouaziz8 <amine.bouaziz@essec.edu>
2021-06-07 18:21:35 +0400

    fix tentative

--- src/App.svelte
+++ src/App.svelte
@@ -25,6 +25,7 @@ Loading..
 {#if error}
 <strong class="text-red-700 font-bold">ERROR! {error} </strong>
 {/if}
+{#if $userStore}
 <header class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
@@ -36,7 +37,6 @@ Loading..
     <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
       <a href="/home" class="mr-5 hover:text-gray-900">Home</a>
     </nav>
-    {#if $userStore}
     <button  on:click={signout} class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Log out
       <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
@@ -115,7 +115,7 @@ Loading..
     h1 {
       width: 100%;
     }
-    #logo {
+    .logo {
       position: relative;
       height: 0;
       padding: 0 0 5% 0;
