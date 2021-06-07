717b1d8693d4a97fa7632fcd234eb71414859a0e
aminebouaziz8 <amine.bouaziz@essec.edu>
2021-06-07 16:45:06 +0400

    log in page

--- src/App.svelte
+++ src/App.svelte
@@ -10,39 +10,30 @@
 </script>
 
 <main>
-  <h1>Svelte + Node.js API</h1>
+  <div id="logo">
+    <picture>
+      <img src="TaxiLogo.jpg" alt="Taxi" width="200"/>
+    </picture>
+  </div>
+  <h1>Welcome to VTC Taxi</h1>
   <h2>
-    Deployed with
-    <a href="https://vercel.com/docs" target="_blank" rel="noreferrer noopener">
-      Vercel
-    </a>
-    !
+    Log in
   </h2>
-  <p>
-    <a
-      href="https://github.com/vercel/vercel/tree/main/examples/svelte"
-      target="_blank"
-      rel="noreferrer noopener">
-      This project
-    </a>
-    is a
-    <a href="https://svelte.dev/">Svelte</a>
-    app with three directories,
-    <code>/public</code>
-    for static assets,
-    <code>/src</code>
-    for components and content, and
-    <code>/api</code>
-    which contains a serverless
-    <a href="https://nodejs.org/en/">Node.js</a>
-    function. See
-    <a href="/api/date">
-      <code>api/date</code>
-      for the Date API with Node.js
-    </a>
-    .
-  </p>
+
   <br />
-  <h2>The date according to Node.js is:</h2>
+
+  <form action="/action_page.php" target="_self">
+    <label for="email">Email:</label>
+    <input type="text" id="email"><br>
+
+    <label for="password">Password:</label>
+    <input type="password" id="password"><br>
+
+
+    <input type="submit" value="Log in">
+  </form>
+  
+  <a href="./register">You don't have an account yet ? Register here</a>
+  <h2>Today is:</h2>
   <p>{date ? date : 'Loading date...'}</p>
 </main>
