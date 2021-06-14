1e01cc55690dbe532549a573cee1e44b2d2bee84
aminebouaziz8 <amine.bouaziz@essec.edu>
2021-06-14 22:41:22 +0400

    fix routing try 1

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,6 @@
 <script>
   import {Router, Link, Routes} from "svelte-routing";
   import Tailwindcss from './Tailwindcss.svelte';
-  import Router from 'svelte-spa-router'
   import SignUp from './routes/SignUp.svelte'
   import SignIn from './routes/SignIn.svelte'
   import Profile from './routes/Profile.svelte'
