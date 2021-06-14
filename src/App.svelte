83301447577ac5278ad8bfdf640647e21b7c9665
BPlpl <83641773+BPlpl@users.noreply.github.com>
2021-06-15 00:06:39 +0200

    Update link contact

--- src/App.svelte
+++ src/App.svelte
@@ -8,7 +8,7 @@
   import Profile from './routes/Profile.svelte'
   import ForgotPassword from './routes/ForgotPassword.svelte'
   import Home from './routes/Home.svelte'
-  import Contact from '.routes/Contact.svelte'
+  import Contact from './routes/Contact.svelte'
   import {userbaseStore, userStore, promiseStore} from './stores'
 
   const routes = {
