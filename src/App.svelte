1f0fa3f2405cc3a4e4d9ada45a0eb6903eb2b2a8
aminebouaziz8 <amine.bouaziz@essec.edu>
2021-06-14 22:56:01 +0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -34,7 +34,7 @@ Loading..
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <Router url="{url}">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<!--<nav class="navbar navbar-expand-lg navbar-light bg-light">
   <img src="/logo.png" alt="Taxi" width="40"/>
   <a class="navbar-brand" >GoTaxi</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -43,38 +43,39 @@ Loading..
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item active">-->
         <Link class="nav-link" to="Home">Home</Link>
-      </li>
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Contact</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
+  <!--</li>
+  <li class="nav-item active">
+    <a class="nav-link" href="#">Contact</a>
+  </li>
+</ul>
+<form class="form-inline my-2 my-lg-0">-->
       <Link class="nav-link" to="Profile">Hello, <!--{$userStore.username}-->! </Link>
-      <button on:click={signout} class="btn btn-outline-danger my-2 my-sm-0" type="submit">Log out</button>
-    </form>
-  </div>
-</nav>
-</Router>
-<div>
-  <Route path="/" component="{Home}"/>
-  <Route path="/" component="{Profile}"/>
+  <!--<button on:click={signout} class="btn btn-outline-danger my-2 my-sm-0" type="submit">Log out</button>
+</form>
 </div>
+</nav>-->
+
+<div>
+<Route path="/" component="{Home}"/>
+<Route path="/" component="{Profile}"/>
+</div>
+</Router>
 <footer class="bg-light text-center text-lg-start">
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2021 Copyright:
-    <a class="text-dark" >VTC Taxi</a>
-  </div>
+<div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+© 2021 Copyright:
+<a class="text-dark" >VTC Taxi</a>
+</div>
 </footer>
 <!--{:else}
 <div class="container flex flex-col justify-center items-center w-screen h-screen mx-auto">
-  <h1>Welcome to THE BEST WAY TO book a Taxi !</h1>
-  <Router routes={{
-  '/': SignUp,
-  '/signin': SignIn,
-  '/forgotpassword': ForgotPassword
-  }} />
+<h1>Welcome to THE BEST WAY TO book a Taxi !</h1>
+<Router routes={{
+'/': SignUp,
+'/signin': SignIn,
+'/forgotpassword': ForgotPassword
+}} />
 </div>
 {/if}
 {/await}-->
