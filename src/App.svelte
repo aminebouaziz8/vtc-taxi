56c8e035c7efdde97fdf7bcac73e7f5836b83405
aminebouaziz8 <amine.bouaziz@essec.edu>
2021-06-15 00:43:42 +0400

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -5,10 +5,12 @@
   import SignIn from './routes/SignIn.svelte'
   import Profile from './routes/Profile.svelte'
   import ForgotPassword from './routes/ForgotPassword.svelte'
+  import Home from './routes/Home.svelte'
   import {userbaseStore, userStore, promiseStore} from './stores'
 
   const routes = {
     // Exact path
+    '/': Home,
     '/signup': SignUp,
     '/signin': SignIn,
     '/forgotpassword': ForgotPassword,
@@ -26,8 +28,8 @@
     () => push('/')
   }
 </script>
-
 <Tailwindcss />
+<Router {routes}/>
 {#await $promiseStore.then(() => Promise.reject())}
 Loading..
 {:catch error}
@@ -36,7 +38,7 @@ Loading..
 {/if}
 
 {#if $userStore}
-<Router {routes}/>
+
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -64,75 +66,9 @@ Loading..
     </form>
   </div>
 </nav>
-<div class="container flex flex-col justify-center items-center w-screen h-screen mx-auto">
-
-
-  <section>
-
-    <div class="row justify-content-center">
-      <div class="col-4">
-        <h1 class="center subtitle" style="text-align: center;">Always on time !</h1>
-      </div>
-    </div>
-
-    <div class="col-16">
-      <h4 style="text-align: center">
-        Whether you’re visiting Paris and want to book a taxi from the train station <strong>to
-        your hotel</strong>, need to get a cab from the city <strong>back home</strong> or are flying out from Paris
-        airports and need an <strong>airport express taxi service</strong> in Paris to get you there as
-        quickly as possible, you can rely on us.
-      </h4>
-    </div>
-    <p style="text-align: center">Order your next ride !</p>
-    <div class="row justify-content-center">
-      <form class="col-16">
-        <div class="form-group">
-          <label for="pickUp">PickUp</label>
-          <input type="text" class="form-control" id="pickUp" placeholder="ex: Cergy">
-        </div>
-        <div class="form-group">
-          <label for="destination">Destination</label>
-          <input type="text" class="form-control" id="destination" placeholder="Ex: La Defense">
-        </div>
-        <div class="form-group">
-          <label for="date">Date</label>
-          <input type="date" id="date" class="form-control" value="2021-31-05" min="today" max="2021-12-31">
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-          <label class="form-check-label" for="inlineRadio1">1 passenger</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-          <label class="form-check-label" for="inlineRadio2">2 passengers</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3">
-          <label class="form-check-label" for="inlineRadio3">3 passengers</label>
-        </div>
-        <div class="form-check form-check-inline">
-          <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-      </form>
-    </div>
-
-  </section>
-
-</div>
-<footer class="bg-light text-center text-lg-start">
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2021 Copyright:
-    <a class="text-dark" >VTC Taxi</a>
-  </div>
-</footer>
 {:else}
 <div class="container flex flex-col justify-center items-center w-screen h-screen mx-auto">
   <h1>Welcome to THE BEST WAY TO book a Taxi !</h1>
-  <Router routes={{
-  '/signup': SignUp,
-  '/signin': SignIn,
-  '/forgotpassword': ForgotPassword
-  }} />
 </div>
 {/if}
 {/await}
