30e6c2ed5a721f8d5993b578f65abab12491abae
BPlpl <83641773+BPlpl@users.noreply.github.com>
2021-06-14 23:57:42 +0200

    Contact form

--- src/routes/Contact.svelte
+++ src/routes/Contact.svelte
@@ -1,78 +1,39 @@
 <script>
-    import Button from '../components/Button.svelte'
-    import Input from '../components/Input.svelte'
-    import A from '../components/A.svelte'
-    import {userbaseStore, userStore, promiseStore} from '../stores'
-    import {push, pop, replace} from 'svelte-spa-router'
+import App from "../App.svelte";
+import A from "../components/A.svelte";
+import { userStore } from "../stores";
+
 </script>
+<div class="container flex flex-col justify-center items-center w-screen h-screen mx-auto">
+<div class="container flex flex-col justify-center items-center w-screen h-screen mx-auto">
 
-<?php
-if($_POST) {
+    <section>
 
-$message=$_POST['name'];
-$message .=$_POST['firstname'];
-$message .=$_POST['user_mail'];
-$message .=$_POST['user_mail'];
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <h1 class="center subtitle" style="text-align: center;">Always on time !</h1>
+            </div>
+        </div>
 
+        <div class="col-16">
+            <h4 style="text-align: center">
+               A question? A feedback? Contact us.
+            </h4>
+        </div>
+        <div class="row justify-content-center">
+            <form class="col-16">
+                <div class="form-group">
+                    <label for="pickUp">Email</label>
+                    <input type="text" class="form-control" id="pickUp" placeholder="ex: Cergy">
+                </div>
+                <div class="form-group">
+                    <label for="subject">Subject</label>
+                    <textarea type="text" class="form-control" id="subject" placeholder="Write something..." style= "height:200px"></textarea></textarea>                </div>
+                <div class="form-check form-check-inline">
+                    <button type="submit" class="btn btn-primary">Send</button>
+                </div>
+            </form>
+        </div>
 
-mail ($message .=$_POST['champtexte2'], "Confirmation of your inscription", $message, "From: contact@taxiteam.com") ; 
-
-}
-?>
-
-<form class = "contact" action="/profile page" method="post">
-    <div class = "contact" >
-        <label  class = "contact" for="name">Name : </label>
-        <input type="text" id="name" name="name">
-    </div>
-     <div class = "contact" >
-        <label  class = "contact" for="firstname">Firstname : </label>
-        <input type="text" id="firstname" name="firstname">
-    </div>
-    <div class = "contact" >
-<input type="file" name="photo" id="photo" /> 
-     </div>
-     <div class = "contact" >
-<input type="submit" class="btn" value="envoyer" /> 
-     </div>
-    <div class = "contact" >
-        <label  class = "contact" for="mail">e-mail :</label>
-        <input type="email" id="mail" name="user_mail">
-    </div>
-    <div class = "contact" >
-        <label  class = "contact" for="phone">phone number: </label>
-        <input type="text " id="phone" name="phone_number">
-    </div>
-<div class = "contact" >
-        <label  class = "contact" for="city">City : </label>
-        <input type="text" id="city" name=”city">
-    </div>
-<div class = "contact" >
-        <label  class = "contact" for="adress">Adress : </label>
-        <input type="text" id="adress" name=“address”>
-    </div>
-</form>
-
-
-<style>
-form.contact {
-  /* Uniquement centrer le formulaire sur la page */
-  margin: 0 auto;
-  width: 400px;
-  /* Encadré pour voir les limites du formulaire */
-  padding: 1em;
-  border: 1px solid #CCC;
-  border-radius: 1em;
-}
-
-div.contact {
-  margin-top: 1em;
-}
-
-label.contact {
-  /* Pour être sûrs que toutes les étiquettes ont même taille et sont correctement alignées */
-  display: inline-block;
-  width: 90px;
-  text-align: right;
-}
-    </style>
+    </section>
+</div>
